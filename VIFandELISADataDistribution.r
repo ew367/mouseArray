730629c44a7d1a627a6cf237e464f2a113275af3
ew367 <E.M.Walker@exeter.ac.uk>
2024-05-22 16:16:41 +0100

    add VIF code

--- VIFandELISADataDistribution.r
+++ VIFandELISADataDistribution.r
@@ -13,14 +13,77 @@
 #----------------------------------------------------------------------#
 
 # https://www.r-bloggers.com/2023/12/exploring-variance-inflation-factor-vif-in-r-a-practical-guide/ 
-
+# https://rforpoliticalscience.com/2020/08/03/check-for-multicollinearity-with-the-car-package-in-r/
+# above says "As a rule of thumb, a vif score over 5 is a problem. A score over 10 should be remedied (and you should consider dropping the problematic variable from the regression model or creating an index of all the closely related variables)."
 
 #----------------------------------------------------------------------#
 # LOAD PACKAGES
 #----------------------------------------------------------------------#
 
-
+library(car)
+library(dplyr)
+library(ggplot2)
 
 #----------------------------------------------------------------------#
 # IMPORT DATA
 #----------------------------------------------------------------------#
+
+load("2_normalised/normalisedData.rdat")
+
+elisa <- read.csv("0_metadata/AB42ELISAconcentrations.csv", stringsAsFactors = F)
+elisa$Individual_ID <- as.character(elisa$Sample_ID)
+elisa$Pathology <- elisa$Mean_N3
+
+QCmetrics <- left_join(QCmetrics, elisa %>% dplyr::select(Individual_ID, Pathology))
+QCmetrics$Group <- as.factor(QCmetrics$Group)
+
+
+
+#----------------------------------------------------------------------#
+# Plot ELISA distribution
+#----------------------------------------------------------------------#
+
+
+ggplot(QCmetrics, aes(x = Pathology, fill = Group))+
+  geom_histogram(position = "dodge")+
+  ggtitle("Elisa concentrations (pathology)")
+
+
+
+#----------------------------------------------------------------------#
+# DEFINE MODELS
+#----------------------------------------------------------------------#
+
+# age as continuous variable (increases possible degrees of freedom compared to factor)
+# missing nuclei number at the moment - add once confirmed by wet lab team
+# age*group here represents pathology
+
+meanBetas <- colMeans(celltypeNormbeta)
+
+# group*age and pathology model
+fullmodel <- lm(meanBetas ~ QCmetrics$Group + QCmetrics$Age*QCmetrics$Group + QCmetrics$Pathology + QCmetrics$Age + QCmetrics$Sex + QCmetrics$Batch) 
+
+vif(fullmodel, type = "predictor")
+
+# no interaction terms
+nullmodel <- lm(meanBetas ~ QCmetrics$Group + QCmetrics$Pathology + QCmetrics$Age + QCmetrics$Sex + QCmetrics$Batch) 
+
+vif(nullmodel)
+
+
+# interaction model for Group*Age
+neunModel<-lm(meanBetas ~ QCmetrics$Group + QCmetrics$Age*QCmetrics$Group + QCmetrics$Age + QCmetrics$Sex + QCmetrics$Batch) 
+
+vif(neunModel, type = "predictor") # need predictor arg to include interaction terms
+
+# null model for Group
+nullneunModel <- lm(meanBetas ~ QCmetrics$Group + QCmetrics$Sex + QCmetrics$Batch +  QCmetrics$Age)
+
+vif(nullneunModel)
+
+
+
+
+
+
+
