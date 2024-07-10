a99e10e4c5fe5c979f72034f109274c084046e9f
ew367 <E.M.Walker@exeter.ac.uk>
2024-07-10 13:34:27 +0100

    edit to include sesame manifest

--- dataExplorationScripts/EpicV1andMouseArrayProbeOverlap.r
+++ dataExplorationScripts/EpicV1andMouseArrayProbeOverlap.r
@@ -18,17 +18,36 @@
 # according to https://www.illumina.com/content/dam/illumina/gcs/assembled-assets/marketing-literature/infinium-mouse-methylation-array-data-sheet-370-2020-002/infinium-mouse-methylation-array-data-sheet-370-2020-002.pdf
 # There is a "Human MethylationEPIC liftover"
 
+
+# https://zwdzwd.github.io/InfiniumAnnotation#mouse this contains a file called "Mouse array design groups (MM285)"
+# which shows the overlap
+
+#   Probe_ID        design                    
+#1 cg00101675_BC21 EPIC;cg00101675           
+#2 cg00116289_BC21 EPIC;cg00116289           
+#3 cg00211372_TC21 EPIC;cg00211372           
+#4 cg00531009_BC21 EPIC;cg00531009           
+#5 cg00747726_TC21 EPIC;cg00747726           
+#6 cg00896209_TC21 CGI;chr3:89169426-89169681
+
 #----------------------------------------------------------------------#
 # IMPORT DATA
 #----------------------------------------------------------------------#
 
 library(CETYGO)
+library(readr)
 
 source("config.r")
 
 #read in manifest and create probe ID with no suffix
 man <- fread(manifest, skip=7, fill=TRUE, data.table=F)
 man$probeID <- gsub("_.*$", "", man$IlmnID)
+
+designMan <- as.data.frame(read_tsv("0_metadata/MM285.design.tsv"))
+epicMatched <- designMan[grep("EPIC", designMan$design),]
+
+epicIDsinMouse <- #
+  ### need to use regexp to extract cg and following 8 digits
 
 
 #----------------------------------------------------------------------#
