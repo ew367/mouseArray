4aac9f7510e008a9947de4508979812ad5514c56
ew367 <E.M.Walker@exeter.ac.uk>
2024-10-11 17:15:12 +0100

    update example config files

--- exampleData/config.r
+++ exampleData/config.r
@@ -1,6 +1,6 @@
 
 # filepaths
-projDir <- "/lustre/projects/Research_Project-191406/cellSortedEWAS"
+projDir <- ""
 pheno <- file.path(projDir, "0_metadata/sampleSheet.csv")
 manifest <- file.path(projDir, "0_metadata/MouseMethylation-12v1-0_A2.csv")
 idatPath <- file.path(projDir, "1_raw/")
@@ -8,8 +8,13 @@ normDir <- file.path(projDir, "2_normalised/")
 QCDir <- file.path(projDir, "2_normalised/QC")
 
 
-# empty wells
-empty <- c("205333480089_R05C02", "205333480089_R06C02")
+# project specific parmeters - must include "Sex" and "Cell_Type" to run optional checks below
+projVar <- c("Genotype", "Sex", "Plate", "Chip_Location") 
+
+
+# optional checks
+ctCheck = FALSE
+sexCheck = TRUE
 
 
 # calcMouseMethMetrics thresholds
@@ -19,15 +24,4 @@ pFiltSampleThresh <- 5
 bsThresh <- 2
 bsConThresh <- 90
 
-
-# optional checks
-ctCheck = TRUE
-sexCheck = TRUE
-
-
-## ctCheck variables
-neunCT <- "NEUNpos"
-predDistinctCT<-c("NEUNpos", "NEUNneg")
-studentThres <- 1.5
-nSDThres<-3
 
