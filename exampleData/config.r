b7d2128cf62264ca53b2a5b827f5cc4a50146825
ew367 <E.M.Walker@exeter.ac.uk>
2024-10-15 10:51:01 +0100

    update example configs

--- exampleData/config.r
+++ exampleData/config.r
@@ -1,20 +1,16 @@
 
 # filepaths
-projDir <- ""
-pheno <- file.path(projDir, "0_metadata/sampleSheet.csv")
-manifest <- file.path(projDir, "0_metadata/MouseMethylation-12v1-0_A2.csv")
-idatPath <- file.path(projDir, "1_raw/")
-normDir <- file.path(projDir, "2_normalised/")
-QCDir <- file.path(projDir, "2_normalised/QC")
-
+manifest <- ("0_metadata/MouseMethylation-12v1-0_A2.csv")
 
 # project specific parmeters - must include "Sex" and "Cell_Type" to run optional checks below
-projVar <- c("Genotype", "Sex", "Plate", "Chip_Location") 
+projVar <- c("Genotype", "Chip_Location", "Sex", "Age") 
+
+# empty <- "" # basenames of any sample to exclude here
 
 
 # optional checks
 ctCheck = FALSE
-sexCheck = TRUE
+sexCheck = FALSE
 
 
 # calcMouseMethMetrics thresholds
